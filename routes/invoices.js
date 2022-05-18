165242b3a0a61657c6d557cb89e5891ec97bf629
Christien Ng <ngchristien@gmail.com>
2022-05-17 19:58:19 -0700

    allow paying of invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -72,21 +72,40 @@ router.post('/', async (req, res, next) => {
  */
 router.put('/:id', async (req, res, next) => {
   try {
-    const { amt, paid, paidDate } = req.body;
+    const { amt, paid } = req.body;
     const { id } = req.params;
+    let paidDate = null;
+
+    const currResult = await db.query(
+      `SELECT paid
+        FROM invoices
+        WHERE id = $1`,
+      [id]
+    );
+
+    if (currResult.rows.length === 0) {
+      throw new ExpressError(`No such invoice: ${id}`, 404);
+    }
+
+    const currPaidDate = currResult.rows[0].paid_date;
+
+    if (!currPaidDate && paid) {
+      paidDate = new Date();
+    } else if (!paid) {
+      paidDate = null;
+    } else {
+      paidDate = currPaidDate;
+    }
 
     const result = await db.query(
       `UPDATE invoices
         SET amt=$1, paid=$2, paid_date=$3
         WHERE id=$4
-        RETURNING id, amt, paid, paid_date`,
+        RETURNING id, comp_code, amt, paid, add_date, paid_date`,
       [amt, paid, paidDate, id]
     );
-    if (result.rows.length === 0) {
-      throw new ExpressError(`No such invoice: ${id}`, 404);
-    } else {
-      return res.json({ invoice: result.rows[0] });
-    }
+
+    return res.json({ invoice: result.rows[0] });
   } catch (e) {
     return next(e);
   }
